e970da295c17e034c1eb23c09674d16e25caf25c
lolpmgo <56176402+lolpmgo@users.noreply.github.com>
2019-10-21 15:10:53 -0700

    students
    students

--- StudentRecordTester.java
+++ StudentRecordTester.java
@@ -14,6 +14,12 @@ public class StudentRecordTester
         int[] c={20,50,50,80};
         
         StudentRecord[] students;
+        
+        students[0] = new StudentRecord[a];
+        students[1] = new StudentRecord[b];
+        students[2] = new StudentRecord[c];
+        
+        
     }
     
 }
